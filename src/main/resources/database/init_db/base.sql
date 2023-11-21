898d290864a0c2ea8a505ccf58d350783c817c33
nomena <vahatra.nomena@yahoo.com>
2023-11-21 10:20:49 +0300

    Update base.sql

--- src/main/resources/database/init_db/base.sql
+++ src/main/resources/database/init_db/base.sql
@@ -1,54 +1,68 @@
-create table code_comptable(
-    id serial primary key,
-    code varchar(100) not null
+create table saisons(
+    idsaison serial primary key,
+    libelle varchar(100) not null,
+    debut timestamp not null,
+    fin timestamp not null
 );
 
-CREATE TYPE ordre_tri AS ENUM ('ASC', 'DESC');
-create table methode_sortie(
-    id serial primary key,
+create table equipes(
+    idequipe serial primary key,
+    nom varchar(100),
+    acronyme varchar(10)
+);
+
+
+create table saison_equipes(
+    idsaisonequipe serial primary key,
+    idsaison int references saisons (idsaison) not null,
+    idequipe int references equipes (idequipe) not null
+);
+
+
+create table joueurs(
+    idjoueur serial primary key,
     nom varchar(100) not null,
-    description ordre_tri
+    prenom varchar(100) not null
 );
 
-create table unite(
-    id serial primary key,
+
+create table contrats(
+    idcontrat serial primary key,
+    idequipe int references equipes (idequipe) not null,
+    idjoueur int references joueurs (idjoueur) not null,
+    debut timestamp not null,
+    fin timestamp not null
+);
+
+
+create table stades(
+    idstade serial primary key,
+    nom varchar(100)
+);
+
+
+create table matchs(
+    idmatch serial primary key,
+    idsaison int references saisons (idsaison) not null,
+    idstade int references stades (idstade) not null,
+    idequipe1 int references equipes (idequipe) not null,
+    idequipe2 int references equipes (idequipe) not null,
+    debut timestamp not null,
+    fin timestamp not null
+);
+
+
+create table type_actions(
+    idtype_action serial primary key,
     nom varchar(100) not null
 );
 
-create table magasin(
-    id serial primary key,
-    nom varchar(100) not null
+
+create table action(
+    idaction serial primary key,
+    idtype int references type_actions (idtype_action) not null,
+    idjoueur int references joueurs (idjoueur) not null,
+    idmatch int references matchs (idmatch) not null,
+    dateheure timestamp not null,
+    valeur int not null
 );
-
-create table article(
-    id serial primary key,
-    nom varchar(100) not null,
-    idMethodeSortie int references methode_sortie(id),
-    idUnite int references unite(id),
-    idCodeComptable int references code_comptable(id)
-);
-
-create table entree_stock(
-    id serial primary key,
-    idArticle int references article(id),
-    idMagasin int references magasin(id),
-    prix_unitaire decimal not null check ( prix_unitaire>0 ),
-    quantite decimal not null check ( quantite>0 ),
-    date_entree_stock timestamp not null default now()
-);
-
-create table entree_stock_pour_sortie(
-    id serial primary key,
-    idEntree_stock int references entree_stock(id),
-    quantite_restante decimal not null check ( quantite_restante>0 ),
-    quantite_cumulative decimal not null check ( quantite_cumulative>0 )
-
-);
-
-create table sortie_stock(
-    id serial primary key,
-    idEntreeStock int references entree_stock(id),
-    quantite decimal not null check ( quantite>0 ),
-    date_sortie_stock timestamp not null default now()
-);
-
