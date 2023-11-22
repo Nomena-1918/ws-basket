1f1889cb12b15b4aa25bca2b926440cafdce83ef
nomena <vahatra.nomena@yahoo.com>
2023-11-22 08:43:23 +0300

    Create CorsConfig.java

---
+++ src/main/java/org/nba/wsbasket/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package org.nba.wsbasket.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
