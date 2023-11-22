c694ef7ca6dcb628bc1afd6dc347d81740d63154
nomena <vahatra.nomena@yahoo.com>
2023-11-22 09:23:16 +0300

    Update CorsConfig.java

--- src/main/java/org/nba/wsbasket/config/CorsConfig.java
+++ src/main/java/org/nba/wsbasket/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
         @Override
         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            registry.addMapping("/**").allowedMethods("*");
         }
 
 }
